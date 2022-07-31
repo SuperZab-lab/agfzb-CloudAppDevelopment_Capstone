fe255ba6390eb0114364a96300f6d6c12269de6a
SuperZab-lab <93475630+SuperZab-lab@users.noreply.github.com>
2022-07-31 13:03:52 +0200

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -64,35 +64,21 @@ def get_dealers_from_cf(url, **kwargs):
 
 # Gets a single dealer from the Cloudant DB with the Cloud Function get-dealerships
 # Requires the dealer_id parameter with only a single value
-def get_dealer_by_id(url, dealer_id):
-    # Call get_request with the dealer_id param
-    json_result = get_request(url, dealerId=dealer_id)
+def get_dealer_by_id_from_cf(url, id):
+    json_result = get_request(url, id=id)
+    print('json_result from line 54',json_result)
 
-    # Create a CarDealer object from response
-    dealer = json_result["entries"][0]
-    dealer_obj = CarDealer(address=dealer["address"], city=dealer["city"], full_name=dealer["full_name"],
-                           id=dealer["id"], lat=dealer["lat"], long=dealer["long"],
-                           short_name=dealer["short_name"],
-                           st=dealer["st"], state=dealer["state"], zip=dealer["zip"])
-
+    if json_result:
+        dealers = json_result["body"]
+        
+    
+        dealer_doc = dealers[0]
+        dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"],
+                                id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],  short_name=dealer["short_name"],full_name=dealer_doc["full_name"],
+                                st=dealer_doc["st"], zip=dealer_doc["zip"])
     return dealer_obj
 
-# Gets all dealers in the specified state from the Cloudant DB with the Cloud Function get-dealerships
-def get_dealers_by_state(url, state):
-    results = []
-    # Call get_request with the state param
-    json_result = get_request(url, state=state)
-    dealers = json_result["body"]["docs"]
-    # For each dealer in the response
-    for dealer in dealers:
-        # Create a CarDealer object with values in `doc` object
-        dealer_obj = CarDealer(address=dealer["address"], city=dealer["city"], full_name=dealer["full_name"],
-                               id=dealer["id"], lat=dealer["lat"], long=dealer["long"],
-                               short_name=dealer["short_name"],
-                               st=dealer["st"], state=dealer["state"], zip=dealer["zip"])
-        results.append(dealer_obj)
 
-    return results
 
 
 # Create a get_dealer_reviews_from_cf method to get reviews by dealer id from a cloud function
